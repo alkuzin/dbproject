dafda913ba5aa3dee9a7bcb01780a26bbf8a7bda
alkuzin <alkuzindev@gmail.com>
2025-04-09 17:48:20 +0300

    feat: added GlobalDB struct

---
+++ src/db/global.rs
@@ -0,0 +1,93 @@
+// DBProject - non-relational databases tasks.
+// Copyright (C) 2025 Alexander (@alkuzin).
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+//! Global database manager related declarations.
+
+use super::{ConnectionConfig, area::{Area, AreaDB}};
+use std::collections::HashMap;
+use sqlx::MySqlPool;
+
+/// Global database manager.
+#[derive(Debug, Default)]
+pub struct GlobalDB {
+    /// Area-specific database managers table.
+    table: HashMap<Area, AreaDB>,
+    /// Global MySQL connection pool.
+    pool: Option<MySqlPool>,
+}
+
+impl GlobalDB {
+    /// Construct new GlobalDB object.
+    ///
+    /// # Returns
+    /// - New `GlobalDB` object.
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    /// Connect global database.
+    ///
+    /// # Parameters
+    /// - `config`  - given MySQL connection config.
+    ///
+    /// # Returns
+    /// - `Ok` - in case of success.
+    /// - `sqlx::Error` - otherwise.
+    pub async fn connect(&mut self, config: &ConnectionConfig)
+        -> Result<(), sqlx::Error>
+    {
+        self.pool = Some(MySqlPool::connect(config.url_db().as_str()).await?);
+        Ok(())
+    }
+
+    /// Insert area-specific database manager.
+    ///
+    /// # Parameters
+    /// - `area_db` - given database manager.
+    /// - `config`  - given MySQL connection config.
+    ///
+    /// # Returns
+    /// - `Ok` - in case of success.
+    /// - `sqlx::Error` - otherwise.
+    pub async fn insert(&mut self, area_db: AreaDB, config: &ConnectionConfig)
+        -> Result<(), sqlx::Error>
+    {
+        // Create database if not exists.
+        if let Some(pool) = &self.pool {
+            let query = format!(
+                "CREATE DATABASE IF NOT EXISTS {}",
+                config.database
+            );
+
+            sqlx::query(query.as_str()).execute(pool).await?;
+        }
+        else {
+            // Handle case when connection pool was not initialized.
+            eprintln!(
+                "Error: connection pool is None: {}",
+                "call GlobalDB::connect() method first!"
+            );
+            return Err(sqlx::Error::PoolClosed)
+        }
+
+        let mut manager = area_db;
+        let url = config.url();
+        manager.connect(url.as_str()).await?;
+
+        self.table.insert(manager.area(), manager);
+        Ok(())
+    }
+}
